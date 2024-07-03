a2b8f291c95e626783efdca2013ed61faab80463
张苗苗 <>
2024-07-03 18:19:18 +0800

    INIT

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { join } from 'path';
+function resolve(dir: string): string {
+  return join(__dirname, dir); //可以用process.cwd() path.join()和path.resolve()
+}
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve('/src'),
+      '#': resolve('/types')
+    }
+  }
+});
